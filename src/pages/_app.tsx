bb40fb52fe16f54f859a0817aca0b0bb0133466c
AzFlin7 <data.alchemist.tech@outlook.com>
2024-11-13 19:44:38 -0600

    remove unused hook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import { AuthProvider } from "@/hooks/useAuth";
-import { ThemeProvider } from "@/hooks/useTheme";
 import type { AppProps } from "next/app";
 import { Flowbite } from "flowbite-react";
 import "@/styles/globals.css";
@@ -7,11 +6,9 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Flowbite>
-      <ThemeProvider>
-        <AuthProvider>
-          <Component {...pageProps} />
-        </AuthProvider>
-      </ThemeProvider>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
     </Flowbite>
   );
 }
